b6d3a9b4248722963edb1bd61472365f45497613
Abhinaya Aryal <aryalabhinaya343@gmail.com>
2023-05-28 15:10:19 +0545

    Refine: character counting

--- 06_character_counting.zig
+++ 06_character_counting.zig
@@ -2,23 +2,21 @@ const std = @import("std");
 const stdin = std.io.getStdIn().reader();
 const stdout = std.io.getStdOut().writer();
 
-pub fn main() !void {
-    var count: u32 = 0;
-    var c = stdin.readByte() catch |err| switch (err) {
+fn readInput(count: u32) !void {
+    _ = stdin.readByte() catch |err| switch (err) {
         error.EndOfStream => {
             try stdout.print("\n\nNo. of characters = {d}\n\n", .{count});
             std.os.exit(0);
         },
         else => |e| return e,
     };
+}
+
+pub fn main() !void {
+    var count: u32 = 0;
+    try readInput(count);
     while (true) {
         count += 1;
-        c = stdin.readByte() catch |err| switch (err) {
-            error.EndOfStream => {
-                try stdout.print("\n\nNo. of characters = {d}\n\n", .{count});
-                std.os.exit(0);
-            },
-            else => |e| return e,
-        };
+        try readInput(count);
     }
 }
