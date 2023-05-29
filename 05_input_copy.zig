d51e3e246c4a67e7b004d784d764e8748bfa002d
Abhinaya Aryal <aryalabhinaya343@gmail.com>
2023-05-29 18:17:28 +0545

    Amazing

--- 05_input_copy.zig
+++ 05_input_copy.zig
@@ -2,11 +2,21 @@ const std = @import("std");
 const stdin = std.io.getStdIn().reader();
 const stdout = std.io.getStdOut().writer();
 
+fn readInput() !*u8 {
+    var c = stdin.readByte() catch |err| switch (err) {
+        error.EndOfStream => {
+            std.os.exit(0);
+        },
+        else => |e| return e,
+    };
+    return &c;
+}
+
 pub fn main() !void {
-    var c = try stdin.readByte();
+    var c = try readInput();
     while (true) {
-        try stdout.writeByte(c);
-        c = try stdin.readByte();
+        try stdout.writeByte(c.*);
+        c = try readInput();
     }
 
     try stdout.writeByte('\n');
